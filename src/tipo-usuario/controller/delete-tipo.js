266b6daace414507559201d30e819ce97935fb43
AyaneYamassaki <karenayane4703@hotmail.com>
2021-06-15 15:42:59 -0300

    Commit - TIPO DE USUARIO CRUDE FUNCIONAL 15/06

---
+++ src/tipo-usuario/controller/delete-tipo.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+
+    $('#tipo').on('click', 'button.btn-delete', function(e) {
+
+        e.preventDefault()
+
+        let IDEIXO = `IDTIPO_USUARIO=${$(this).attr('id')}`
+
+        Swal.fire({
+            title: 'Library',
+            text: 'Deseja realmente excluir esse registro?',
+            icon: 'question',
+            showCancelButton: true,
+            confirmButton: 'Sim',
+            cancelButton: 'Não'
+        }).then((result => {
+            if (result.value) {
+
+                $.ajax({
+                    type: 'POST',
+                    dataType: 'json',
+                    assync: true,
+                    data: IDEIXO,
+                    url: 'src/tipo-usuario/model/delete-tipo.php',
+                    success: function(dados) {
+                        Swal.fire({
+                            title: 'Library',
+                            text: dados.mensagem,
+                            icon: dados.tipo,
+                            confirmButtonText: 'OK'
+                        })
+
+                        $('#tipo').DataTable().ajax.reload()
+                    }
+                })
+            }
+        }))
+
+    })
+})
