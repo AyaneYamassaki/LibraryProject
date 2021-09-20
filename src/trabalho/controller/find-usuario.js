e8d3bf88e41c957aaa30819c6cae6596c7dff251
AyaneYamassaki <karenayane4703@hotmail.com>
2021-09-20 18:46:32 -0300

    Ultimas atualizações find user

--- src/trabalho/controller/find-usuario.js
+++ src/trabalho/controller/find-usuario.js
@@ -1,3 +1,20 @@
+function listAuthor() {
+
+    $('.alert').click(function(e) {
+        e.preventDefault()
+        let id = $(this).attr('data-id')
+        let nome = $(this).attr('data-name')
+
+        $('#listar').append(`
+                                <div class="alert alert-primary">${nome}</div>
+                                <input type="hidden" name="USUARIO_IDUSUARIO" value="${id}"/>
+                            `)
+        $('#' + id).hide()
+
+
+    })
+}
+
 $('document').ready(function() {
 
     $('#AUTOR').keyup(function(e) {
@@ -5,10 +22,8 @@ $('document').ready(function() {
 
         let NOME = `NOME=${$(this).val()}`
 
-        $('#autores').empty()
-        $('#listar').empty()
-
         if ($(this).val().length >= 3) {
+            $('#autores').empty()
             $.ajax({
                 dataType: 'json',
                 type: 'POST',
@@ -17,15 +32,15 @@ $('document').ready(function() {
                 url: 'src/usuario/model/find-usuario.php',
                 success: function(dados) {
                     for (const dado of dados) {
-                        $('#autores').append(`<input type="text" name="" id="${dado.IDUSUARIO}" class="form-control selecionado" value="${dado.NOME}">`)
+                        // Pode-se usar tanto id="" & name="" como também data-id=""" & data-name=""
+                        $('#autores').append(`<div data-name="${dado.NOME}" data-id="${dado.IDUSUARIO}" class="alert alert-secondary">${dado.NOME}</div>`)
+                            // div não é editável e por isso colocar uma div
                     }
-                    $('.selecionado').click(function(e) {
-                        $('#autores').empty()
-                        $('#AUTOR').empty()
-                        $('#listar').append(`<input type="text" name="" id="" class="form-control selecionado" value="">`)
-                    })
+                    listAuthor()
                 }
             })
+        } else {
+            $('#autores').empty()
         }
 
     })
